89febaeda5ab2b8ac5bd70ed51c487a4bd3fe7d7
Ahmet <aeldra995@hotmail.com>
2023-09-15 11:22:27 +0300

    #UrunController Backend and Frontend Finished.

---
+++ TeknikMarket.CoreMVCUI/Areas/Admin/Controllers/UrunController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeknikMarket.CoreMVCUI.Areas.Admin.Filter;
+
+namespace TeknikMarket.CoreMVCUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [RolFilter("Admin")]
+    public class UrunController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
